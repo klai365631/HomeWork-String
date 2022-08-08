2766910e47fac5dc03c0ba499e10bac9bfc8217d
ANDRE <klai1@mail.ru>
2022-08-08 17:08:39 +0400

    Сделал 3 дz

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello word");
+//        Задача 1
+        String firstName = "Ivan";
+        String middleName = "Ivanov";
+        String lastName = "Ivanovich";
+        String fullName = middleName+' ' + firstName+' ' + lastName;
+        System.out.println("ФИО сотрудника — " + fullName);
+//        Задача 2
+        fullName = fullName.toUpperCase();
+        System.out.println("fullName = " + fullName);
+//        Задача 3
+        String fullName = "Иванов Семён Семёнович";
+        fullName = fullName.replace("ё", "е");
+        System.out.println("fullName = " + fullName);
+
+
     }
 }
